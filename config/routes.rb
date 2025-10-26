23a4fad5cb6fa559a84a18728a80414622f71ff7
Jugal Kishor <115132207+Jugalkishor1@users.noreply.github.com>
2025-10-26 16:22:43 +0530

    Add index and show pages for Quizes

--- config/routes.rb
+++ config/routes.rb
@@ -11,4 +11,5 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   root "home#index"
   resources :exams, only: [:index, :show]
+  resources :quizzes, only: [:index, :show]
 end
