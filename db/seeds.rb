0b113cba5c0a74aad996b5746a963588ea077d05
Jugal Kishor <115132207+Jugalkishor1@users.noreply.github.com>
2025-11-05 21:32:15 +0530

    Setup render

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,3 +8,5 @@
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
 AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
+
+AdminUser.create!(email: "#{ENV['ADMIN_EMAIL']}", password: "#{ENV['ADMIN_PASS']}", password_confirmation: "#{ENV['ADMIN_PASS']}") if Rails.env.production?
